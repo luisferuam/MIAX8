f933572258aff3c6cebfc6d69b2b91ee3660a370
luisferuam <70706406+luisferuam@users.noreply.github.com>
2022-07-18 10:52:11 +0200

    Delete optimizers.py

--- pykeras/src/optimizers.py
+++
@@ -1,9 +0,0 @@
-# La clase Optimizer bla bla
-
-class Optimizer(object):
-
-    def __init__(self, learning_rate):
-        self.learning_rate = learning_rate
-
-    def apply(self, net):
-        pass
